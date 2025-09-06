226427e0475011cc7d9f646b4c5a96c53fa393c0
Shaneshwar Bhagat <shaneshwarb02@hotmail.com>
2025-09-07 00:32:56 +0530

    alibaba config added

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [{
+    name: "KFT-systems-webui-alibaba",
+    script: "npm",
+    args: "start",
+    cwd: "/home/KFT-systems-webui",
+    env: {
+      NODE_ENV: "production",
+      HOST: "0.0.0.0",
+      PORT: 3001
+    }
+  }]
+}
